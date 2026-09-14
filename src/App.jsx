7d9aed6bd5e21d2eeb82eec472500280d61f38d2
Milena Montes <milena.magali@gmail.com>
2026-09-14 16:09:30 -0300

    Instalación de DOM e Implementación de  rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <ItemListContainer greeting="¡Bienvenidos a Pet Shop Manada!"/>
-      <ItemDetailContainer />
-    </>
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<ItemListContainer />} />
+
+        <Route path="/category/:id" element={<ItemListContainer />} />
+
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+
+        <Route path="*" element={<h2>Página no encontrada</h2>} />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
